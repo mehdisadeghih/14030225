6bbbba67eaac5fac4c46083255576f8e67904329
mehdisadeghih <mehdiisadeghii.2001@gmail.com>
2024-05-14 20:43:24 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,120 @@
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
+const music = document.querySelector("audio");
+const progressContainer = document.getElementById("progress-container");
+const progress = document.getElementById("progress");
+const currentTimeEle = document.getElementById("current-time");
+const durationEle = document.getElementById("duration");
+const prevBtn = document.getElementById("prev");
+const playBtn = document.getElementById("play");
+const nextBtn = document.getElementById("next");
+
+// فراخوانی موزیک ها
+const songs = [
+    {
+        name: "01",
+        displayName: "جهان لاغر",
+        artist: "محسن چاووشی",
+    },
+    {
+        name: "02",
+        displayName: "بارون بارونه",
+        artist: "سینا درخشنده",
+    },
+    {
+        name: "03",
+        displayName: "درست نمیشم",
+        artist: "سیروان خسروی",
+    },
+];
+
+let isPlaying = false;
+
+function loadSong(song) {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+}
+
+let songIndex = Math.floor(Math.random() * songs.length);
+loadSong(songs[songIndex]);
+
+function setSongDuration(e) {
+    const totalSeconds = Math.floor(e.target.duration);
+    const durationMinutes = Math.floor(totalSeconds / 60);
+    let durationSeconds = totalSeconds % 60;
+    if (durationSeconds < 10) {
+        durationSeconds = `0${durationSeconds}`;
+    }
+    durationEle.textContent = `${durationMinutes}:${durationSeconds}`;
+}
+
+function playSong() {
+    isPlaying = true;
+    playBtn.classList.replace("fa-play", "fa-pause");
+    playBtn.setAttribute("title", "Pause");
+    music.play();
+}
+
+function pauseSong() {
+    isPlaying = false;
+    playBtn.classList.replace("fa-pause", "fa-play");
+    playBtn.setAttribute("title", "Play");
+    music.pause();
+}
+
+function prevSong() {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex]);
+    progress.style.width = `0%`;
+    playSong();
+}
+
+function nextSong() {
+    songIndex++;
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    progress.style.width = `0%`;
+    playSong();
+}
+
+function barWidthAndCurrentTime() {
+    const { duration, currentTime } = music;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+    const currentMinutes = Math.floor(currentTime / 60);
+    let currentSeconds = Math.floor(currentTime % 60);
+    if (currentSeconds < 10) {
+        currentSeconds = `0${currentSeconds}`;
+    }
+    currentTimeEle.textContent = `${currentMinutes}:${currentSeconds}`;
+}
+
+function updateProgressBar() {
+    if (isPlaying) {
+        barWidthAndCurrentTime();
+    }
+}
+
+function setProgressBar(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const { duration } = music;
+    music.currentTime = (clickX / width) * duration;
+    if (!isPlaying) {
+        barWidthAndCurrentTime();
+    }
+}
+
+music.addEventListener("canplay", setSongDuration);
+playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+music.addEventListener("timeupdate", updateProgressBar);
+music.addEventListener("ended", nextSong);
+progressContainer.addEventListener("click", setProgressBar);
